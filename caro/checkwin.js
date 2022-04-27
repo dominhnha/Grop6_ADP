ac2a050a0298322baa0c571bea754a2ac0393935
Phamtheduyet123 <100422317+Phamtheduyet123@users.noreply.github.com>
2022-04-27 19:06:49 +0700

    updata__checkWin
    updata__checkWin

---
+++ caro/checkwin.js
@@ -0,0 +1,42 @@
+function checkWin() {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] == "x") {
+                // ktra hang ngang
+                if (arr[i][j + 1] == "x" && arr[i][j + 2] == "x") {
+                    return true;
+                }
+                // ktra hang doc
+                if (arr[i + 1][j] == "x" && arr[i + 2][j] == "x") {
+                    return true;
+                }
+                // ktra dong-nam
+                if (arr[i + 1][j + 1] == "x" && arr[i + 2][j + 2] == "x") {
+                    return true;
+                }
+                // Ktra tay-bac
+                if (arr[i + 1][j - 1] == "x" && arr[i + 2][j - 2] == "x") {
+                    return true;
+                }
+            }
+            if (arr[i][j] == "o") {
+                // ktra hang ngang
+                if (arr[i][j + 1] == "o" && arr[i][j + 2] == "o") {
+                    return true;
+                }
+                // ktra hang doc
+                if (arr[i + 1][j] == "o" && arr[i + 2][j] == "o") {
+                    return true;
+                }
+                // ktra dong-nam
+                if (arr[i + 1][j + 1] == "o" && arr[i + 2][j + 2] == "o") {
+                    return true;
+                }
+                // Ktra tay-bac
+                if (arr[i + 1][j - 1] == "o" && arr[i + 2][j - 2] == "o") {
+                    return true;
+                }
+            }
+        }
+    }
+}
